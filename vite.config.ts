eb26286245cc34ca9349f01e1d432b30425e0b28
lucky74 <lucky.jamaludin@gmail.com>
2026-01-20 03:27:56 +0700

    Setup PWA for App Installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo_mitas.png'],
+      manifest: {
+        name: 'E-Book Ramadhan Abadi',
+        short_name: 'RamadhanBook',
+        description: 'Kisah Kejayaan Islam & Jejak Peradaban',
+        theme_color: '#020617',
+        background_color: '#020617',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo_mitas.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo_mitas.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
